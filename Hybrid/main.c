e50d87311f573eab8d0744c1cb7139aeff043871
DHjerpe <hjerpedaniel@gmail.com>
2017-05-25 17:08:13 +0200

    struct instead of long array!

--- Hybrid/main.c
+++ Hybrid/main.c
@@ -11,11 +11,31 @@
 
 int NPROCS;
 
-/* Five input arguments:
- 1: N = number of stars                                         (argv[1])
- 2: nsteps = number of time steps                               (argv[2])
- 3: delta_t = size of time step                                 (argv[3])
- 4: VERIFY, 0 = NO, 1 = YES (Default)                           (argv[4])
+/**
+ struct to store information about each particle
+ */
+typedef struct particle
+{
+    double x_pos; // x-component (position)
+    double y_pos; // y-component (position)
+    double mass;  // mass of the particle
+    double x_vel; // x-component (velocity)
+    double y_vel; // y-component (velocity)
+} particle;
+
+void forceCalc(particle * particles, int N,double delta_t, int nsteps) {
+void forceCalcVerify(particle * particles, int N,double delta_t, int nsteps);
+void generateStars(particle * particles, int size);
+void printErrorMsg();
+    
+/**
+ Main function of the program. That simulates the N-body problem.
+ Initialises and finalises MPI.
+ Params:
+    - N: number of stars                                         (argv[1])
+    - nsteps: number of time steps                               (argv[2])
+    - delta_t: = size of time step                               (argv[3])
+    - verify: 0 = NO, 1 = YES                                    (argv[4])
  */
 int main(int argc, char** argv) {
     
@@ -24,17 +44,22 @@ int main(int argc, char** argv) {
         double wtime_end;
         double wtime_start = MPI_Wtime();
         int my_rank;
-        int N = atoi(argv[1])*5;
+        //int N = atoi(argv[1])*5;
+        int N = atoi(argv[1]);
         int nsteps = atoi(argv[2]);
-	int verify = atoi(argv[4]);
+        int verify = atoi(argv[4]);
         double delta_t = atof(argv[3]);
         
-        double * p = NULL;
-        p = (double *)malloc(sizeof(double)*N);
+      //  double * p = NULL;
+     //   p = (double *)malloc(sizeof(double)*N);
+       
         
+        particle * particle = NULL;
+        particle = (particle *)malloc(sizeof(particle)*N);
         
+        generateStars(particles, N);
         
-        generateStars(p, N/5);
+        //generateStars(p, N/5);
         
         
         MPI_Init(&argc, &argv);
@@ -43,7 +68,9 @@ int main(int argc, char** argv) {
         MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
         
         
-        forceCalc(p,N,delta_t,nsteps);
+       // forceCalc(p,N,delta_t,nsteps);
+        
+        forceCalc(particles, N, delta_t, nsteps);
         
         
         MPI_Finalize();
@@ -51,18 +78,30 @@ int main(int argc, char** argv) {
         
         if (verify && my_rank == 0) {
             
-            double * p_ref = NULL;
-            p_ref = (double *)malloc(sizeof(double)*N);
-            generateStars(p_ref, N/5);
+//            double * p_ref = NULL;
+//            p_ref = (double *)malloc(sizeof(double)*N);
+//            generateStars(p_ref, N/5);
             
-            forceCalcVerify(p_ref,N,delta_t,nsteps);
+            double * particles_ref = NULL;
+            particles_ref = (particle *)malloc(sizeof(particle));
+            generateStars(particles_ref, N);
+            
+           // forceCalcVerify(p_ref,N,delta_t,nsteps);
+            forceCalcVerify(particles_ref, N, delta_t, nsteps);
             
             for (int i = 0; i<N/5; i++) {
-                assert(p[i] == p_ref[i]);
-                assert(p[i+1] == p_ref[i+1]);
-                assert(p[i+2] == p_ref[i+2]);
-                assert(p[i+3] == p_ref[i+3]);
-                assert(p[i+4] == p_ref[i+4]);
+//                assert(p[i] == p_ref[i]);
+//                assert(p[i+1] == p_ref[i+1]);
+//                assert(p[i+2] == p_ref[i+2]);
+//                assert(p[i+3] == p_ref[i+3]);
+//                assert(p[i+4] == p_ref[i+4]);
+                
+                assert(particles.x_pos == particles_ref.x_pos);
+                assert(particles.y_pos == particles_ref.y_pos);
+                assert(particles.mass == particles_ref.mass);
+                assert(particles.x_vel == particles_ref.x_vel);
+                assert(particles.y_vel == particles_ref.y_vel);
+                
             }
             
             printf("Verify OK! \n");
@@ -86,9 +125,10 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-void forceCalc(double * p, int N,double delta_t, int nsteps) {
-  
-    N = N/5;
+//void forceCalc(double * p, int N,double delta_t, int nsteps) {
+    void forceCalc(particle * particles, int N,double delta_t, int nsteps) {
+
+   // N = N/5;
     double G = 100.0/N;
     
     int upperLimit = N;
@@ -106,12 +146,13 @@ void forceCalc(double * p, int N,double delta_t, int nsteps) {
         }
  
     
-    int numberToSend = (upperLimit - lowerLimit)*5;
+   // int numberToSend = (upperLimit - lowerLimit)*5;
+    int numberToSend = (upperLimit - lowerLimit);
 
-
-    double * p_buffer = NULL;
-    p_buffer = (double *)malloc(sizeof(double)*N*5);
-    memcpy(p_buffer,p,sizeof(double)*N*5);
+    double * particles_buffer = NULL;
+    particles_buffer = (double *)malloc(sizeof(double)*N*5);
+    memcpy(particles_buffer,particles,sizeof(particle)*N);
+        
     
     
     for (int n = 0; n<nsteps; n++) {
@@ -132,13 +173,13 @@ void forceCalc(double * p, int N,double delta_t, int nsteps) {
             for (int j = 0; j<N; j++) {
                 
                 if (i != j) {
-                    x_diff = (p[i*5]-p[j*5]);     // rij (vector), x-component
-                    y_diff = (p[i*5+1]-p[j*5+1]); // rij (vector), y-component
+                    x_diff = (particles[i].x_pos - particles[j].x_pos);
+                    y_diff = (particles[i].y_pos - particles[j].y_pos);
                     
                     r = sqrt((x_diff*x_diff) + (y_diff*y_diff));
                     
-                    mass = p[j*5+2];
-                    mass /= ((r+EPSILON)*(r+EPSILON)*(r+EPSILON)); // not mass. total constant
+                    mass = p[j].mass;
+                    mass /= ((r+EPSILON)*(r+EPSILON)*(r+EPSILON)); // apply plummer constant
                     
                     x_diff *= mass;
                     y_diff *= mass;
@@ -152,42 +193,55 @@ void forceCalc(double * p, int N,double delta_t, int nsteps) {
             a_x = -G*sumX;
             a_y = -G*sumY;
             
-            // veloceties
-            u_x = p[i*5+3] + delta_t*a_x;
-            u_y = p[i*5+4] + delta_t*a_y;
+            // calculate new veloceties
+            u_x = particles[i].x_vel + delta_t*a_x;
+            u_y = particles[i].y_vel + delta_t*a_y;
             
-            // update veloceties
-            p_buffer[i*5+3] = u_x;
-            p_buffer[i*5+4] = u_y;
+            // update new veloceties
+            particles_buffer[i].x_vel = u_x;
+            particles_buffer[i].y_vel = u_y;
             
-            // update position
-            p_buffer[i*5] = p[i*5] + delta_t*u_x;
-            p_buffer[i*5+1] = p[i*5+1] + delta_t*u_y;
+            // update new position
+            particles_buffer[i].x_pos = particles[i].x_pos + delta_t*u_x;
+            particles_buffer[i].y_pos = particles[i].y_pos + delta_t*u_y;
         }
         
         }
         
-        MPI_Allgather(MPI_IN_PLACE, numberToSend, MPI_DOUBLE, p_buffer, numberToSend, MPI_DOUBLE, MPI_COMM_WORLD);
+        MPI_Allgather(MPI_IN_PLACE, numberToSend, MPI_DOUBLE, particles_buffer, numberToSend, MPI_DOUBLE, MPI_COMM_WORLD);
         
         
         // copy the new values to p
-        memcpy(p,p_buffer,sizeof(double)*N*5);
+        memcpy(particles,particles_buffer,sizeof(particle)*N);
         
     }
     
-    free(p_buffer);
-    p_buffer = NULL;
+    //free(p_buffer);
+  //  p_buffer = NULL;
+        
+        free(particles_buffer);
+        particles_buffer = NULL;
 }
 
+/**
+ A vanilla function (serial), that computes a reference 
+ to the above parallel implementation
+ */
+//void forceCalcVerify(double * p, int N,double delta_t, int nsteps) {
+void forceCalc(particle * particles, int N,double delta_t, int nsteps) {
 
-void forceCalcVerify(double * p, int N,double delta_t, int nsteps) {
-    
-    N = N/5;
+   // N = N/5;
     double G = 100.0/N;
     
-    double * p_buffer = NULL;
-    p_buffer = (double *)malloc(sizeof(double)*N*5);
-    memcpy(p_buffer,p,sizeof(double)*N*5);
+//    double * p_buffer = NULL;
+//    p_buffer = (double *)malloc(sizeof(double)*N*5);
+//    memcpy(p_buffer,p,sizeof(double)*N*5);
+//    
+    
+    double * particles_buffer = NULL;
+    particles_buffer = (double *)malloc(sizeof(double)*N*5);
+    memcpy(particles_buffer,particles,sizeof(particle)*N);
+    
     
     
     for (int n = 0; n<nsteps; n++) {
@@ -205,13 +259,13 @@ void forceCalcVerify(double * p, int N,double delta_t, int nsteps) {
                 for (int j = 0; j<N; j++) {
                     
                     if (i != j) {
-                        x_diff = (p[i*5]-p[j*5]);     // rij (vector), x-component
-                        y_diff = (p[i*5+1]-p[j*5+1]); // rij (vector), y-component
+                        x_diff = (particles[i].x_pos - particles[j].x_pos);
+                        y_diff = (particles[i].y_pos - particles[j].y_pos);
                         
                         r = sqrt((x_diff*x_diff) + (y_diff*y_diff));
                         
-                        mass = p[j*5+2];
-                        mass /= ((r+EPSILON)*(r+EPSILON)*(r+EPSILON)); // not mass. total constant
+                        mass = p[j].mass;
+                        mass /= ((r+EPSILON)*(r+EPSILON)*(r+EPSILON)); // apply plummer constant
                         
                         x_diff *= mass;
                         y_diff *= mass;
@@ -224,31 +278,40 @@ void forceCalcVerify(double * p, int N,double delta_t, int nsteps) {
                 a_x = -G*sumX;
                 a_y = -G*sumY;
                 
-                // veloceties
-                u_x = p[i*5+3] + delta_t*a_x;
-                u_y = p[i*5+4] + delta_t*a_y;
+                // calculate new veloceties
+                u_x = particles[i].x_vel + delta_t*a_x;
+                u_y = particles[i].y_vel + delta_t*a_y;
                 
-                // update veloceties
-                p_buffer[i*5+3] = u_x;
-                p_buffer[i*5+4] = u_y;
+                // update new veloceties
+                particles_buffer[i].x_vel = u_x;
+                particles_buffer[i].y_vel = u_y;
                 
-                // update position
-                p_buffer[i*5] = p[i*5] + delta_t*u_x;
-                p_buffer[i*5+1] = p[i*5+1] + delta_t*u_y;
+                // update new position
+                particles_buffer[i].x_pos = particles[i].x_pos + delta_t*u_x;
+                particles_buffer[i].y_pos = particles[i].y_pos + delta_t*u_y;
             }
     
         // copy the new values to p
-        memcpy(p,p_buffer,sizeof(double)*N*5);
+       // memcpy(p,p_buffer,sizeof(double)*N*5);
+         memcpy(p,p_buffer,sizeof(particle)*N);
     }
     
-    free(p_buffer);
-    p_buffer = NULL;
+    //free(p_buffer);
+    //p_buffer = NULL;
+    
+    free(particles_buffer);
+    particles_buffer = NULL;
+    
 }
 
 
 
 /**
- p is a vector containing information about all stars/particles in the system
+ Generate particles (stars) at random position, with random masses,
+ random positions, and random velocities
+ 
+ Params:
+    - p: vector containing information about all stars/particles in the system
  p contains: 
  - x position
  - y position
@@ -256,7 +319,7 @@ void forceCalcVerify(double * p, int N,double delta_t, int nsteps) {
  - x velocity
  - y velocity
  
- N is the total number of stars/particles
+    - N: the total number of stars/particles
  */
 void generateStars(double * p, int N) {
     
@@ -278,6 +341,12 @@ void generateStars(double * p, int N) {
     }
 }
 
+
+/**
+ Function that prints error message if 
+ the number of input arguments (from the command line)
+ to the program is wrong
+ */
 void printErrorMsg() {
     
     printf("Error, wrong number of input arguments.\nThe input arguments should have the following form:  \n");
--- Hybrid/main.h
+++
@@ -1,4 +0,0 @@
-void printErrorMsg();
-void forceCalc(double * p, int N,double delta_t, int nsteps);
-void generateStars(double * p, int size);
-void forceCalcVerify(double * p, int N,double delta_t, int nsteps);
